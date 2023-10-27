a1c67109a38fd10962d6da8bc8685399714ea9ab
Jeffrey <JeffreyPG03@gmail.com>
2023-10-27 12:38:42 -0400

    additional progress

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -20,6 +20,8 @@ import Image from 'next/image';
 import { ChangeEvent, useState } from 'react';
 import { isBase64Image } from '@/lib/utils';
 import { useUploadThing } from '@/lib/uploadthing'
+import { updateUser } from '@/lib/actions/user.actions';
+import { usePathname, useRouter } from 'next/navigation';
 
 interface Props {
     user: {
@@ -37,6 +39,8 @@ const AccountProfile = ({ user, btnTitle}: Props) => {
     
     const [files, setFiles] = useState<File[]>([]) 
     const { startUpload } = useUploadThing("media");
+    const router = useRouter();
+    const pathname = usePathname();
 
     const form = useForm({
         resolver: zodResolver(UserValidation),
@@ -85,7 +89,20 @@ const AccountProfile = ({ user, btnTitle}: Props) => {
             }
         }
 
-        // TODO: Upadte user profile
+        await updateUser({
+            userId: user.id,
+            path: pathname,
+            username: values.username,
+            name: values.name,
+            bio: values.bio,
+            image: values.profile_photo,
+        });
+
+        if(pathname === '/profile/edit') {
+            router.back();
+        } else {
+            router.push('/');
+        }
     }
 
     return (
